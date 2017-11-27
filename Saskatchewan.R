ee1198fc4d0389ed0fb7a98e0fc5f334e231442d
Henry <ahenry@ualibrary.ualberta.ca>
2017-11-27 15:00:55 -0700

    Saskatchewan data

---
+++ Saskatchewan.R
@@ -0,0 +1,47 @@
+##Install and load needed packages
+
+##install.packages("bibliometrix", dependencies = TRUE)
+##install.packages("splitstackshape")
+##install.packages("tidyverse")
+
+library(bibliometrix)
+library(splitstackshape)
+library(tidyverse)
+library(stringr)
+
+## Read in downloaded files and convert to dataframe
+filePathsSask = dir("./Saskatchewan", pattern = "*.bib", recursive = TRUE, full.names = TRUE) 
+DSask <- do.call("readFiles", as.list(filePathsSask)) 
+MSask <- convert2df(DSask, dbsource = "scopus", format = "bibtex")
+
+## Keep only selected columns: UT, DT, C1, DT, TC, PY
+mydataSask <- select(MSask, UT, C1, DT, PY, TC)
+
+## Separate authors into single observations
+tidy_dataSask <- cSplit(mydataSask, "C1", sep = ";", direction = "long")
+
+##Test that there were no unintended drops
+count <- sum(str_count(mydataSask$C1, ";"))
+ifelse(count + nrow(mydataSask) == nrow(tidy_dataSask), "No drops", "Warning") 
+
+## Remove non-Saskatchewan addresses
+SaskData <- tidy_dataSask[grep("UNIVERSITY OF SASKATCHEWAN", tidy_dataSask$C1), ]
+
+deptURL <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSwbFiCTwa2Kd5r62CK04AC7iRfLTDSt18PWe76bO4QSKGqpHkb0I7ZeSesCZN_wGN_NVytVi0x0h1J/pub?gid=1900927404&single=true&output=csv"
+depts <- read.csv(deptURL)
+
+abs <- as.character(depts$Abbreviation)
+dept_test <- sapply(SaskData$C1, function(x) abs[str_detect(x, abs)])
+
+engData<-cbind(SaskData,plyr::ldply(dept_test,rbind)[,1])
+names(engData)[6]<-"Abbreviation"
+engDeptData <- merge(engData, depts, all.x = TRUE) ##keeps nonmatches and enters NA
+
+## check the "other"s for articles that should be kept
+Other <- filter(engDeptData, is.na(Department))
+View(Other)
+
+## ##Keep only eng departments and output data to file
+finalEngData <- engDeptData[complete.cases(engDeptData), ]
+engDataDD <- unique(select(finalEngData, UT, DT, TC, PY, Department))
+write.csv(engDataDD, "Saskatchewan.csv", quote = TRUE, row.names = FALSE)
